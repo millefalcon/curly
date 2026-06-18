fe8d97e4d71d334ffd0f3798d22bfffb459a9f29
millefalcon <hanish0019@gmail.com>
2026-06-17 23:05:01 -0400

    Last known proper build

--- setup.py
+++ setup.py
@@ -6,16 +6,11 @@ setup(
     ext_modules=[
         Extension(
             name="pycurly",
-            include_dirs = subprocess.run(["python3-config", "--cflags"], capture_output=True, text=True).stdout.split(), #  [],
-            # libraries = ["curl"],
-            library_dirs = subprocess.run(["python3-config", "--ldflags"], capture_output=True, text=True).stdout.split() + ["/usr/lib/x86_64-linux-gnu/"],
+            include_dirs = subprocess.run(["python3-config", "--cflags"], capture_output=True, text=True).stdout.split(),
+            libraries = ["curl"],
+            library_dirs = subprocess.run(["python3-config", "--ldflags"], capture_output=True, text=True).stdout.split(),
             sources=["curly.c"],
             extra_link_args=[
-                "-Wl,-Bstatic",
-                "-lcurl",           # This library will be linked statically
-                "-lcrypto",
-                "-Wl,-Bdynamic",   # ALL subsequent libraries (like gcc_s) link dynamically
-                "-lgssapi_krb5",
             ],
         ),
     ]
